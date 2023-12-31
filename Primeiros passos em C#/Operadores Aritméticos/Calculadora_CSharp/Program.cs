5c76ae190cd9873f4cae5234e5e09d9e722d24d2
LucasTMB <lucas.tmb708@gmail.com>
2023-12-30 22:24:30 -0300

    Criando uma calculadora que faz as operações aritméticas básicas, além da potenciação, seno, coseno, tangente e raiz quadrada

---
+++ Primeiros passos em C#/Operadores Aritméticos/Calculadora_CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculadora_CSharp.Models;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(5, 8);
+calc.Subtrair(3, 1);
+calc.Multiplicar(4, 4);
+calc.Dividir(4, 2);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(9);
