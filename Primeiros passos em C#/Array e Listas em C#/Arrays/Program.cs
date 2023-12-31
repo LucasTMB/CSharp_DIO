e285d81e4ffe98145d76552d985817abf0a955f5
LucasTMB <lucas.tmb708@gmail.com>
2023-12-31 00:56:27 -0300

    Iniciando os estudos com arrays

---
+++ Primeiros passos em C#/Array e Listas em C#/Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] arrayInteiros = new int[3]; // definindo o tamanho do array em 3 itens
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+// arrayInteiros[3] = 1 // vai quebrar, pois é acima do tamanho definido
+
+for(int contador = 0; contador < arrayInteiros.Length; contador++) {
+    Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
+}
